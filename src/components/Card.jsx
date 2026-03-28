4ba025364635105cf5b1fa188c133a49e99468cc
Esther Ndosi <estherndosi24@gmail.com>
2026-03-28 12:00:30 +0300

    Load image and style image card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 function Card() {
-    return (
-        <div className="card">
-            <img />
-            <span>Cow</span>
-        </div>
-    )
+  const [image, setImage] = useState(null);
+
+  console.log(import.meta.env.VITE_PEXELS_KEY);
+
+  fetch("https://api.pexels.com/v1/photos/5130821", {
+    headers: {
+      Authorization: import.meta.env.VITE_PEXELS_KEY,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => setImage(data))
+    .catch((reason) => console(reason));
+
+  return (
+    <div className="card">
+      {image && <img src={image.src.medium} alt={image.photographer} />}
+      <span>Cow</span>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
